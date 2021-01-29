b61612d04e37e04f6ae7b17cbebecf8ace6802b2
raulvsh <raulvsh@gmail.com>
2021-01-29 01:26:58 +0100

    El fondo se repite y vallas se mueven a la izquierda. Se destruyen las vallas. Añadido game over al chocar. Añadidas animaciones de correr y salto

---
+++ Assets/Course Library/Scripts/ControlJugador.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class ControlJugador : MonoBehaviour
+{
+    private Rigidbody rbJugador;
+    private Animator animJugador;
+
+    public float fuerzaSalto = 10;
+    public float modGravedad = 2;
+    public bool estaEnElSuelo = true;
+    public bool gameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbJugador = GetComponent<Rigidbody>();
+        Physics.gravity *= modGravedad;
+        animJugador = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && estaEnElSuelo)
+        {
+            //transform.Translate(Vector3.up * Time.deltaTime);
+            rbJugador.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
+            estaEnElSuelo = false;
+            animJugador.SetTrigger("Jump_trig");
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Suelo"))
+        {
+            estaEnElSuelo = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstaculo"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+        }
+    }
+}
